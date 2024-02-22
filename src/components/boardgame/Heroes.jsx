81be8d3eb40c5f3480c2d93417125ac3bf3bab48
Rpayet <robinpayet@gmail.com>
2024-02-22 16:03:34 +0100

    Refactor Heroes component

--- src/components/boardgame/Heroes.jsx
+++ src/components/boardgame/Heroes.jsx
@@ -1,32 +1,24 @@
 import { useState } from "react";
+import { TbUserPentagon } from "react-icons/tb";
 import { GiDefensiveWall, GiBroadsword  } from "react-icons/gi";
 
 export default function Heroes({ hero, player }) {
 
     const [crown, setCrown] = useState(hero.crown.bronze);
     const [bulwark, setBulwark] = useState(hero.bulwark.bronze);
-    const [rod, setRod] = useState(2);
+    const [rod, setRod] = useState(player.diamond.rod);
     const [energy_to_act, setEnergy_to_act] = useState(hero.energy_to_act.bronze);
     const [exp, setExp] = useState(player.diamond.exp);
-
+    
   return (
     <div id='Heroes' className={hero.name}>
-        <h1 className='title'>{hero.name}</h1>
+        <h1 className=''><TbUserPentagon /> {hero.name}</h1>
+        <meter min='0' max='6' value={exp}></meter>
         <div className=''>
-            <p className='exp'>xp</p>
-            <meter min='0' max='6' value={exp}></meter>
+            <p><GiBroadsword /> {crown}</p>
+            <p><GiDefensiveWall /> {bulwark}</p>
         </div>
-        <div className=''>
-            <div>
-                <p><GiBroadsword /> {crown}</p>
-            </div>
-            <div>
-                <p><GiDefensiveWall /> {bulwark}</p>
-            </div>
-        </div>
-        <div className='rod'>
-            <meter min='0' max={energy_to_act} value={rod} />
-        </div>
+        <meter min='0' max={energy_to_act} value={rod} />
     </div>
   );
   
