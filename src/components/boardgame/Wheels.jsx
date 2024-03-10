c1e32b905af1367c76d8fe750c3be341bb5c8f34
Rpayet <robinpayet@gmail.com>
2024-03-10 16:52:11 +0100

    Refactor Wheels component and update wheel icons

--- src/components/boardgame/Wheels.jsx
+++ src/components/boardgame/Wheels.jsx
@@ -1,14 +1,16 @@
+import { Container, Sprite, Stage } from "@pixi/react";
 import { useState } from "react";
 import { FaDiamond, FaSquare, FaHammer } from "react-icons/fa6";
+import wheelSpriteSheet from '../../assets/images/sprites/Wheels_sprite-sheet_HD.png';
 
 export default function Wheels() {
 
     const wheels= {
-        wheel1: ['S', 'D', 'S', 'S+', 'D', 'H', 'DD+', 'H'],
-        wheel2: ['S+', 'D', 'SS', 'D+', 'S', 'H', 'DD', 'HH'],
-        wheel3: ['S+', 'D', 'D+', 'S', 'D', 'HH', 'SS', 'HH'],
-        wheel4: ['S', 'D', 'S+', 'D', 'HH', 'S',' D+', 'HH'],
-        wheel5: ['S', 'D', 'H', '', '', 'S', 'D', ''],
+        wheel1: ['s1', 'd1', 's1', 'sp1', 'd1', 'd1', 'dp2', 'd1'],
+        wheel2: ['sp1', 'd1', 's2', 'dp1', 's1', 'd1', 'd2', 'd2'],
+        wheel3: ['sp1', 'd1', 'dp1', 's1', 'd1', 'd2', 's2', 'd2'],
+        wheel4: ['s1', 'd1', 'sp1', 'd1', 'd2', 's1',' dp1', 'd2'],
+        wheel5: ['s1', 'd1', 'd1', 'blank', 'blank', 's1', 'd1', 'blank'],
     };
 
     const handleSpin = () => {
@@ -22,6 +24,23 @@ export default function Wheels() {
     };
 
     const [spinResult, setSpinResult] = useState([]);
+
+    const iconParticularsDict = {
+        's1': {x: 0, y: 0},
+        's2': {x: -102.5, y: 0},
+        's3': {x: -205, y: 0},
+        'sp1': {x: -307.5, y: 0},
+        'sp2': {x: -410, y: 0},
+        'd1': {x: 0, y: -77.5},
+        'd2': {x: -102.5, y: -77.5},
+        'd3': {x: -205, y: -77.5},
+        'dp1': {x: -307.5, y: -77.5},
+        'dp2': {x: -410, y: -77.5},
+        'h1': {x: 0, y: -155},
+        'h2': {x: -102.5, y: -155},
+        'h3': {x: -205, y: -155},
+        'blank': {x: -307.5, y: -155},
+    };
 
     const iconDisplay = (icon) => {
         switch(icon) {
@@ -59,6 +78,10 @@ export default function Wheels() {
                     return <span key={index}>{iconDisplay(icon)}</span>
                 })}
             </div>
+
+            <Stage width={102.5} height={77.5} options={{ backgroundAlpha: 0 }}>
+                <Sprite image={wheelSpriteSheet} {...iconParticularsDict.blank} anchor={0} scale={.25} />
+            </Stage>
 
             <button onClick={() => handleSpin()} type="button">Spin</button>
         </div>
