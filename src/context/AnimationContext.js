60e93a9a24b9fbbe42c8b22b64fdf2131ecba628
Rpayet <robinpayet@gmail.com>
2024-07-11 22:33:18 +0200

    Setup page transition handler

--- src/context/AnimationContext.js
+++ src/context/AnimationContext.js
@@ -5,13 +5,15 @@ export const AnimationContext = createContext();
 export const AnimationProvider = ({children}) => {
 
     const [mkPageAnim, setMkPageAnim] = useState(false);
+    const [landingPageAnim, setLandingPageAnim] = useState(false);
 
-    const animationContextValue = {
-        mkPageAnim, setMkPageAnim
+    const animations = {
+        mkPage: [mkPageAnim, setMkPageAnim],
+        landingPage: [landingPageAnim, setLandingPageAnim],
     }
 
     return (
-        <AnimationContext.Provider value={animationContextValue}>
+        <AnimationContext.Provider value={{animations}}>
             {children}
         </AnimationContext.Provider>
     )
--- src/services/navigation/animationService.js
+++ src/services/navigation/animationService.js
@@ -1,13 +1,23 @@
+import { useContext } from "react";
+import { AnimationContext } from "../../context/AnimationContext";
 import { useHistory } from "react-router-dom";
 
 export const usePageTransition = () => {
 
+    const { animations } = useContext(AnimationContext);
     const history = useHistory();
 
-    const navigateWithAnimation = (fromPage, toPage, timeout) => {
+    const navigateWithAnimation = (fromPage, toPage, timeout=500) => {
         if (fromPage === toPage) return;
 
+        const [_, setFromPageAnim] = animations[fromPage];
+        const [__, setToPageAnim] = animations[toPage];
 
+        setFromPageAnim(false);
+        setTimeout(() => {
+            setToPageAnim(true);
+            history.push(`/${toPage}`);
+        }, timeout);
     };
 
     return { navigateWithAnimation };
