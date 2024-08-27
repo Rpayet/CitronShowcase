cade0385f5198467bea9d270a10a15e6868b3072
Rpayet <robinpayet@gmail.com>
2024-08-27 10:20:23 +0200

    WIP Automatic Language Localization (suspended)

--- src/services/server/callerService.js
+++ src/services/server/callerService.js
@@ -1,8 +1,24 @@
+import { config } from '@react-spring/web';
 import axios from 'axios';
 
 const Axios = axios.create({
     baseURL: "http://localhost:3333",
     withCredentials: true,
+})
+
+// Axios.interceptors.request.use(config => {
+//     const token = localStorage.getItem('token');
+//     if (token) {
+//         config.headers.Authorization = `Bearer ${token}`;
+//     }
+//     return config;
+// }
+// );
+
+Axios.interceptors.request.use(config=> {
+    const userLanguage = navigator.language || navigator.userLanguage;
+    config.headers['Accept-Language'] = userLanguage;
+    return config;
 })
 
 export default Axios;
