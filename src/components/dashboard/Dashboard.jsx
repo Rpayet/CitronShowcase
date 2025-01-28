de6b2e72d1bf44e68be8b9824054530a0d580249
Rpayet <robinpayet@gmail.com>
2025-01-28 19:46:06 +0100

    dashboard wip

--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -6,79 +6,94 @@ import { AppContext } from "../../context/AppProvider";
 
 export default function Dashboard() {
 
+    const fromPage = window.location.pathname.split('/')[1];
     const { handleNavigation } = usePageTransition();
     const { appTheme } = useContext(AppContext);
 
-    const dashboardLink = [
-        {id: 'landing', to:'', name: 'Lemonify', light: [0, -640], dark:[-64, -640]},
-        {id: 'articles', to:'articles', name: 'Articles', light: [-256, -640], dark:[-320, -640]},
-        {id: 'portfolio', to:'portfolio', name: 'Portefolio', light: [-512, -640], dark:[-576, -640]},
-        {id: 'arcadePalace', to:'arcadePalace', name: 'Arcade Palace', light: [-768, -640], dark:[-832, -640]},
-    ];
+    const dashboardLink = {
+        landing : {id: 'landing', to:'', name: 'Lemonify', light: [0, -640], dark:[-64, -640]},
+        articles : {id: 'articles', to:'articles', name: 'Articles', light: [-256, -640], dark:[-320, -640]},
+        portfolio : {id: 'portfolio', to:'portfolio', name: 'Portefolio', light: [-512, -640], dark:[-576, -640]},
+        arcadePalace : {id: 'arcadePalace', to:'arcadePalace', name: 'Arcade Palace', light: [-768, -640], dark:[-832, -640]},
+    }
+
+    const [linkArray, setLinkArray] = useState([dashboardLink.articles, dashboardLink.portfolio, dashboardLink.arcadePalace]);
 
     const [dashStatus, setDashStatus] = useState('');
 
-    const [selected, setSelected] = useState('Page');
+    const [selected, setSelected] = useState(fromPage);
     const [hovered, setHovered] = useState('');
     
-    const fromPage = window.location.pathname.split('/')[1];
     useEffect(() => {
         if (fromPage === '') {
             setDashStatus('hidden');
         } else {
             setDashStatus('show');
         }
+        setSelected(fromPage);
+        if (fromPage !== '') {
+            setLinkArray((prevArray) => {
+                const selectedLink = dashboardLink[fromPage];
+                const filteredArray = prevArray.filter(link => link.id !== fromPage);
+                return [selectedLink, ...filteredArray];
+            });
+        }
     }, [fromPage]);
+
         
     return (
         <div id="Dashboard" className={dashStatus}>
-            <div className="header">
-                <button className="dashLink" onClick={() => handleNavigation('')}>
-                    <div className="headerIcon">
-                        <Stage
-                            width={64}
-                            height={64}
-                            options={{backgroundAlpha: 0}}>
-                                <Sprite
-                                    image={lemonifylogoset}
-                                    position={dashboardLink[0][appTheme]}
-                                    anchor={[0, 0]}
-                                    scale={1}
-                                />
-                        </Stage>
-                    </div>
-                    <p className="linkname">{dashboardLink[0].name}</p>
-                </button>
+            <div 
+                className={`header`}
+                onClick={() => handleNavigation('')} >
+                    <button className="dashLink">
+                        <div className="headerIcon">
+                            <Stage
+                                width={64}
+                                height={64}
+                                options={{backgroundAlpha: 0}}>
+                                    <Sprite
+                                        image={lemonifylogoset}
+                                        position={dashboardLink.landing[appTheme]}
+                                        anchor={[0, 0]}
+                                        scale={1}
+                                    />
+                            </Stage>
+                        </div>
+                        <p className="linkname">{dashboardLink.landing.name}</p>
+                    </button>
             </div>
             <div className="dashBody">
                 <div className="nav">
-                    {dashboardLink.slice(1).map((link) => {
+                    {linkArray.map((link) => {
                         return (
                             <button
                                 key={link.id}
                                 id={link.id}
-                                className={`dashlink`}
-                                onClick={() => handleNavigation(link.to) } >
-                                    <div className="navIcon">
+                                className={`dashLink ${hovered === link.name ? 'focus' : 'unfocus' } ${selected === link.id ? 'selected' : 'unselected'}`}
+                                onClick={() => handleNavigation(link.to) } 
+                                onMouseEnter={() => setHovered(link.name)}
+                                onMouseLeave={() => setHovered('')}>
+                                    <div className={`navIcon ${selected === link.id ? '' : 'reduce'}`}>
                                         <Stage                        
                                             width={64}
                                             height={64}
                                             options={{backgroundAlpha: 0}}>
                                                 <Sprite
                                                     image={lemonifylogoset}
-                                                    position={link[appTheme]}
+                                                    position={selected === link.id ? link.light : link.dark}
                                                     anchor={[0, 0]}
                                                     scale={1}                                   
                                                 />
                                         </Stage>
                                     </div>
-                                    {/* <p className="linkname">{link.name}</p> */}
                             </button>
                         );
                     })}
+                    <p className={`linkname ${hovered !== '' ? 'show' : ''}`}>{hovered}</p>
                 </div>
                 <div className="dashBodyContent">
-                    <h2 className="section">{selected.toUpperCase()}</h2>
+                    <h2 className="section">{linkArray.find(link => link.id === selected)?.name}</h2>
                     <p className="description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. </p>
                     <div>
                         <p>Sous-lien</p>
