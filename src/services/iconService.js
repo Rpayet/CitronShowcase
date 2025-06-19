d5c5042a9ff24ba57ddf4b203289a6610b241a73
Rpayet <robinpayet@gmail.com>
2025-06-19 13:23:31 +0200

    Refactor icon positioning logic; enhance comments for clarity and improve handling of 256px icons

--- src/services/iconService.js
+++ src/services/iconService.js
@@ -1,11 +1,21 @@
+// Custom hook to handle icon positioning logic
 export const useIcon = () => {
+    // Function to calculate icon position based on type, id, and theme
     const handleIconPosition = (type, id, theme) => {
+        // Mapping of icon sizes to their Y-axis positions
         const yArray = { '256': 0, '128': 512, '64': 640, '32': 704, '16': 736 };
+        
+        // Special handling for 256px icons
         if (type === 256) {
-            return [(256 * id), (!theme ? 0 : 256)];
+            // Returns X and Y positions for 256px icons
+            return [(256 * (id  ? 0 : 7 )), (!theme ? 0 : 256)]; // Ignore id "0" for 256px icons, replace with 7 for empty icon
         };
-        return [`-${((256 * id) + (!theme ? (type - type) : type))}`,`-${yArray[type]}`];
+        
+        // Returns X and Y positions for other icon sizes
+        // X position is negative to shift the icon, Y position is based on yArray
+        return [`-${((256 * id) + (!theme ? (type - type) : type))}`, `-${yArray[type]}`];
     };
 
+    // Expose the handleIconPosition function
     return { handleIconPosition };
-} 
+}
